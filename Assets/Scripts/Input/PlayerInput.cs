7f0123d26bf6868edee221f974b198132f2778f6
Max Okorokov <maxim.okorokov@gmail.com>
2018-08-08 23:20:09 +0300

    Fingers added. Basic Camera Movement Added

---
+++ Assets/Scripts/Input/PlayerInput.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using DigitalRubyShared;
+
+public class PlayerInput : MonoBehaviour {
+
+    public Camera cam;
+
+    private PanGestureRecognizer panGesture;
+    private TapGestureRecognizer tapGesture;
+    private ScaleGestureRecognizer scaleGesture;
+
+    void Start () {
+        CreateTapGesture();
+        CreatePanGesture();
+        CreateScaleGesture();
+
+        panGesture.AllowSimultaneousExecution(scaleGesture);
+
+        // prevent the one special no-pass button from passing through,
+        //  even though the parent scroll view allows pass through (see FingerScript.PassThroughObjects)
+        FingersScript.Instance.CaptureGestureHandler = CaptureGestureHandler;
+
+        // show touches, only do this for debugging as it can interfere with other canvases
+        FingersScript.Instance.ShowTouches = true;
+    }
+
+    private void CreateTapGesture()
+    {
+        tapGesture = new TapGestureRecognizer();
+        tapGesture.StateUpdated += TapGestureCallback;
+        FingersScript.Instance.AddGesture(tapGesture);
+    }
+
+    private void TapGestureCallback(GestureRecognizer gesture)
+    {
+        if (gesture.State == GestureRecognizerState.Ended)
+        {
+            DebugText("Tapped at {0}, {1}", gesture.FocusX, gesture.FocusY);
+        }
+    }
+
+    private void CreatePanGesture()
+    {
+        panGesture = new PanGestureRecognizer();
+        panGesture.MinimumNumberOfTouchesToTrack = 2;
+        panGesture.StateUpdated += PanGestureCallback;
+        FingersScript.Instance.AddGesture(panGesture);
+    }
+
+    private void PanGestureCallback(GestureRecognizer gesture)
+    {
+        if (gesture.State == GestureRecognizerState.Executing)
+        {
+            DebugText("Panned, Location: {0}, {1}, Delta: {2}, {3}", gesture.FocusX, gesture.FocusY, gesture.DeltaX, gesture.DeltaY);
+
+            float deltaX = panGesture.DeltaX / 25.0f;
+            float deltaY = panGesture.DeltaY / 25.0f;
+            Vector3 pos = cam.transform.position;
+            pos.x += deltaX;
+            pos.y += deltaY;
+            cam.transform.position = pos;
+        }
+    }
+
+    private void CreateScaleGesture()
+    {
+        scaleGesture = new ScaleGestureRecognizer();
+        scaleGesture.StateUpdated += ScaleGestureCallback;
+        FingersScript.Instance.AddGesture(scaleGesture);
+    }
+
+    private void ScaleGestureCallback(GestureRecognizer gesture)
+    {
+        if (gesture.State == GestureRecognizerState.Executing)
+        {
+            DebugText("Scaled: {0}, Focus: {1}, {2}", scaleGesture.ScaleMultiplier, scaleGesture.FocusX, scaleGesture.FocusY);
+            cam.orthographicSize *= scaleGesture.ScaleMultiplier;
+        }
+    }
+
+    private static bool? CaptureGestureHandler(GameObject obj)
+    {
+        // I've named objects PassThrough* if the gesture should pass through and NoPass* if the gesture should be gobbled up, everything else gets default behavior
+        if (obj.name.StartsWith("PassThrough"))
+        {
+            // allow the pass through for any element named "PassThrough*"
+            return false;
+        }
+        else if (obj.name.StartsWith("NoPass"))
+        {
+            // prevent the gesture from passing through, this is done on some of the buttons and the bottom text so that only
+            // the triple tap gesture can tap on it
+            return true;
+        }
+
+        // fall-back to default behavior for anything else
+        return null;
+    }
+
+    private void DebugText(string text, params object[] format)
+    {
+        //bottomLabel.text = string.Format(text, format);
+        Debug.Log(string.Format(text, format));
+    }
+
+    private void LateUpdate()
+    {
+        int touchCount = Input.touchCount;
+        if (FingersScript.Instance.TreatMousePointerAsFinger && Input.mousePresent)
+        {
+            touchCount += (Input.GetMouseButton(0) ? 1 : 0);
+            touchCount += (Input.GetMouseButton(1) ? 1 : 0);
+            touchCount += (Input.GetMouseButton(2) ? 1 : 0);
+        }
+        string touchIds = string.Empty;
+        int gestureTouchCount = 0;
+        foreach (GestureRecognizer g in FingersScript.Instance.Gestures)
+        {
+            gestureTouchCount += g.CurrentTrackedTouches.Count;
+        }
+        foreach (GestureTouch t in FingersScript.Instance.CurrentTouches)
+        {
+            touchIds += ":" + t.Id + ":";
+        }
+    }
+}
